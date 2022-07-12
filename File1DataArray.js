eb7f69beeb95e2b62de5b85dea85399a745f8e74
Donn10 <109121509+Donn10@users.noreply.github.com>
2022-07-12 14:29:45 +0700

    Add files via upload

---
+++ File1DataArray.js
@@ -0,0 +1,9 @@
+//create array
+    var data1 = [1, 1, 1, 2, 2, 4, 1, 1 ];
+    var data2 = [2, 1, 2, 2, 2, 8, 1, 1 ];
+    var data3 = [3, 3, 3, 3, 2, 4, 1, 1 ];
+    
+    let val;
+    //get length
+    val=number.length;
+console.log(val)
