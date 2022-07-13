31481892d3f1f435bd44aabba862442131b07e90
Donn10 <109121509+Donn10@users.noreply.github.com>
2022-07-13 14:12:37 +0700

    Add files via upload

--- File1DataArray.js
+++ File1DataArray.js
@@ -1,9 +1,36 @@
-//create array
-    var data1 = [1, 1, 1, 2, 2, 4, 1, 1 ];
-    var data2 = [2, 1, 2, 2, 2, 8, 1, 1 ];
-    var data3 = [3, 3, 3, 3, 2, 4, 1, 1 ];
-    
-    let val;
-    //get length
-    val=number.length;
-console.log(val)
+/* 
+Array1; [1, 1, 1, 2, 2, 4, 1, 1 ]
+Array2; [2, 1, 2, 2, 2, 8, 1, 1 ];
+Array3; [3, 3, 3, 3, 2, 4, 1, 1 ];
+*/
+var arNums1; [1, 1, 1, 2, 2, 4, 1, 1 ]
+var arNums2; [2, 1, 2, 2, 2, 8, 1, 1 ];
+var arNums3; [3, 3, 3, 3, 2, 4, 1, 1 ];
+var arNums4 = {
+  ... var arNums1
+  ... var arNums2
+  ... var arNums3
+}
+console.log(var arNums4)
+
+var occurances = {};
+var curNumber;
+var maxNumber = arNums[0];
+for(let i=0; i<arNums.length; i++) {
+    curNumber = arNums [i];
+
+    if(occurances[curNumber] !== undefined) {
+        occurances[curNumber]++;
+    }else {
+        occurances[curNumber] = 1;
+    }
+    if(occurances[curNumber]> occurances[maxNumber]) {
+        maxNumber = curNumber;
+    }
+}
+if(occurances[maxNumber]> arNums.length/2){
+    writeline("Found the majority element: " + maxNumber);
+
+} else{
+    writeline("No Majority element found");
+}
